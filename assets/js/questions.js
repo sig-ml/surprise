2b772b304eaf627e704f9bb73e10716b14d34db3
Arjun S <arjun.mi1@iiitmk.ac.in>
2017-08-01 23:56:24 +0530

    Push minor updates

---
+++ assets/js/questions.js
@@ -0,0 +1,49 @@
+function hideEveryQ() {
+  $('#Ballon_Breaking').hide();
+  $('#Durvasav').hide();
+  $('#Boys_Oppana').hide();
+  $('#ass').hide();
+  $('#Surprise').hide();
+  $('#Tongue_Twisters').hide();
+  $('#First_Night').hide();
+  $('#Folk_Dance').hide();
+  $('#Dhinchak_Pooja').hide();
+  $('#Ujala_Ad').hide();
+  $('#Nagavalli_Dance').hide();
+  $('#Meenkari').hide();
+  $('#Dubsmash').hide();
+  $('#Bus-stop_Poovalan').hide();
+  $('#Ballon_Hole').hide();
+  $('#bear').hide();
+  $('#Cat-Walk').hide();
+  $('#Despacito').hide();
+  $('#Happy_Song_Sad').hide();
+  $('#Film-Classical_Song').hide();
+  $('#Thiruvathira').hide();
+  $('#Blind-rope Jumping').hide();
+  $('#Toilet_Cleaning').hide();
+  $('#Belly_Dancing').hide();
+  $('#Mustard_game').hide();
+  $('#Mixed-Dance').hide();
+  $('#Velichapaadu').hide();
+  $('#Chali-Malayalam').hide();
+  $('#Dead-Body').hide();
+  $('#Belt_Game').hide();
+  $('#Driving_Lesson').hide();
+  $('#Action_Song').hide();
+  $('#Push_Up').hide();
+  $('#Santosh_Pandit_Dialogue').hide();
+  $('#Mimicking_Sound').hide();
+  $('#Music_with_Body-Muscles').hide();
+  $('#Mimic').hide();
+  $('#Cheer-girls').hide();
+  $('#Margamkali').hide();
+}
+
+function preloading() {
+  $('#intermediate').hide();
+}
+
+$('#intermediate').show();
+hideEveryQ();
+setTimeout(preloading,2000);
