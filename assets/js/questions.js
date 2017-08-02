8f9a0f55d23c77eb9be996db4de7f2b88f9b4738
Arjun S <arjun.mi1@iiitmk.ac.in>
2017-08-02 11:18:22 +0530

    updated final tasks list, names pending

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,49 +1,118 @@
-function hideEveryQ() {
-  $('#Ballon_Breaking').hide();
-  $('#Durvasav').hide();
-  $('#Boys_Oppana').hide();
-  $('#ass').hide();
-  $('#Surprise').hide();
-  $('#Tongue_Twisters').hide();
-  $('#First_Night').hide();
-  $('#Folk_Dance').hide();
-  $('#Dhinchak_Pooja').hide();
-  $('#Ujala_Ad').hide();
-  $('#Nagavalli_Dance').hide();
-  $('#Meenkari').hide();
-  $('#Dubsmash').hide();
-  $('#Bus-stop_Poovalan').hide();
-  $('#Ballon_Hole').hide();
-  $('#bear').hide();
-  $('#Cat-Walk').hide();
-  $('#Despacito').hide();
-  $('#Happy_Song_Sad').hide();
-  $('#Film-Classical_Song').hide();
-  $('#Thiruvathira').hide();
-  $('#Blind-rope Jumping').hide();
-  $('#Toilet_Cleaning').hide();
-  $('#Belly_Dancing').hide();
-  $('#Mustard_game').hide();
-  $('#Mixed-Dance').hide();
-  $('#Velichapaadu').hide();
-  $('#Chali-Malayalam').hide();
-  $('#Dead-Body').hide();
-  $('#Belt_Game').hide();
-  $('#Driving_Lesson').hide();
-  $('#Action_Song').hide();
-  $('#Push_Up').hide();
-  $('#Santosh_Pandit_Dialogue').hide();
-  $('#Mimicking_Sound').hide();
-  $('#Music_with_Body-Muscles').hide();
-  $('#Mimic').hide();
-  $('#Cheer-girls').hide();
-  $('#Margamkali').hide();
+document.getElementById('1').classList.remove("super_hide");
+var i = 1;
+var time = 3000;
+var string1 = i + 'n';
+var string = '#' + i;
+var string2 = '#' + i + 'n';
+function randomNum() {
+  return (Math.floor((Math.random() * 5) + 1)*1000)
+}
+function hideNames() {
+  //task
+  var string = '#' + i;
+  //current task name
+  var string1 = i + 'n';
+  //previous task name
+  var string2 = (i-1) + 'n';
+  if ($(string).hasClass("super_hide")) {
+      // document.getElementById(string1).classList.add("super_hide");
+  } else {
+      document.getElementById(string2).classList.add("super_hide");
+  }
+}
+function showNames() {
+    //task
+    var string = '#' + i;
+    //current task name
+    var string1 = i + 'n';
+    //previous task name
+    var string2 = (i-1) + 'n';
+    console.log("String:"+string);
+    console.log("String1:"+string1);
+    if ($(string).hasClass("super_hide")) {
+      console.log(i);
+        // document.getElementById(string1).classList.add("super_hide");
+    } else {
+        document.getElementById(string1).classList.remove("super_hide");
+        document.getElementById(string2).classList.add("super_hide");
+    }
+}
+function hideLoading() {
+  document.getElementById('preloader').classList.remove("active");
+}
+function showLoading() {
+  document.getElementById('preloader').classList.add("active");
+}
+function hideNames2() {
+  //task
+  var string = '#' + i;
+  //current task name
+  var string1 = i + 'n';
+  //previous task name
+  var string2 = (i+1) + 'n';
+
+  if ($(string).hasClass("super_hide")) {
+      // document.getElementById(string1).classList.add("super_hide");
+  } else {
+      document.getElementById(string2).classList.add("super_hide");
+  }
+}
+function showNames2() {
+    //task
+    var string = '#' + i;
+    //current task name
+    var string1 = i + 'n';
+    //previous task name
+    var string2 = (i+1) + 'n';
+
+    if ($(string).hasClass("super_hide")) {
+      console.log(i);
+        // document.getElementById(string1).classList.add("super_hide");
+    } else {
+        document.getElementById(string1).classList.remove("super_hide");
+        document.getElementById(string2).classList.add("super_hide");
+    }
+}
+time = randomNum();
+setTimeout(hideLoading,time);
+setTimeout(showNames,time);
+
+
+function nextPage() {
+    if (i == 40) {
+        window.location = "index5.html"
+    }
 }
 
-function preloading() {
-  $('#intermediate').hide();
+function previousPage() {
+    if (i == 0) {
+        window.location = "index3.html"
+    }
 }
 
-$('#intermediate').show();
-hideEveryQ();
-setTimeout(preloading,2000);
+function increment() {
+  document.getElementById('preloader').classList.add("active");
+    document.getElementById(i).classList.add("super_hide");
+    i = i + 1;
+    document.getElementById(i).classList.remove("super_hide");
+    hideNames();
+    time = randomNum();
+    setTimeout(hideLoading,time);
+    setTimeout(showNames,time);
+    nextPage()
+}
+
+function decrement() {
+    showLoading();
+    document.getElementById(i).classList.add("super_hide");
+    console.log(i);
+    i = i - 1;
+    if (i > 1) {
+        document.getElementById(i).classList.remove("super_hide");
+        hideNames2();
+        time = randomNum();
+        setTimeout(hideLoading,time);
+        setTimeout(showNames2,time)
+    }
+    previousPage()
+}
